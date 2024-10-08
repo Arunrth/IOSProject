6965e0b110c77f1159b519d32fbb571c45d872f1
Arunrth <arunrthwm@gmail.com>
2024-10-08 22:26:39 +0530

    image and text view

--- IosProject/ContentView.swift
+++ IosProject/ContentView.swift
@@ -9,63 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+
+        
         ZStack{
-            Color(.white)
+            Color(.black)
                 .ignoresSafeArea()
-//            VStack{
-//                Image("arunPic")
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fit)
-//                    .cornerRadius(20)
-//                    .padding(10)
-//                Text("Arun Rathore")
-//                    .font(.title)
-//                    .fontWeight(.semibold)
-//                    .foregroundColor(Color.green)
-//                
-//                
-//            }
             
-            VStack(alignment: .leading, spacing: 11.0){
+            VStack{
                 Image("arunPic")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .cornerRadius(10)
-                HStack{
-                    Text("Arun Rathore")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.green)
+                    .cornerRadius(30)
+                Text("Arun Rathore")
+                    .font(.title)
+                    .foregroundColor(.green)
                     
-                    Spacer()
-                    
-                    VStack(alignment: .leading){
-                        HStack{
-                            Image(systemName: "star.fill")
-                            Image(systemName: "star.fill")
-                            Image(systemName: "star.fill")
-                            Image(systemName: "star.fill")
-                            Image(systemName: "star.lefthalf.fill")
-                                
-                        }
-                       
-                        Text("Review 242")
-                    }
-                    .foregroundColor(Color.orange)
-                    .font(.caption)
-
-                }
-               
-                Text("Photo taken by DSLR Camera Pro")
-                    .foregroundColor(Color.yellow)
-                }
-            .padding()
-            .background(Rectangle()
-                .foregroundColor(.white)
-                .cornerRadius(15)
-                .shadow(radius: 15)
-            )
-            .padding()
+            }
         }
        
     }
