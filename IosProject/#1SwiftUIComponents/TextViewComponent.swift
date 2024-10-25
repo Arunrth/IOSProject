7cb8cf94b630c70a191d1909491234f21da0101a
Arunrth <arunrthwm@gmail.com>
2024-10-25 16:51:17 +0530

    Text component

---
+++ IosProject/#1SwiftUIComponents/TextViewComponent.swift
@@ -0,0 +1,18 @@
+//
+//  TextViewComponent.swift
+//  IosProject
+//
+//  Created by Arun Rathore on 25/10/24.
+//
+
+import SwiftUI
+
+struct TextViewComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextViewComponent()
+}
