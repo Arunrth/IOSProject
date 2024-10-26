b964423bc4330f57da1d98faf473f59f819c6f3d
Arunrth <arunrthwm@gmail.com>
2024-10-26 16:58:47 +0530

    Ellipse view with modifier

--- IosProject/#1SwiftUIComponents/#3EllipseShape.swift
+++ IosProject/#1SwiftUIComponents/#3EllipseShape.swift
@@ -5,13 +5,19 @@
 //  Created by Arun Rathore on 26/10/24.
 //
 
+// Ellipse same properties as circle
+
 import SwiftUI
 
 struct _EllipseShape: View {
     var body: some View {
         Ellipse()
 //            .fill(.blue)
-            .stroke(.blue, style: StrokeStyle(lineWidth: 10))
+//            .foregroundColor(.green)
+            .trim(from:0.3, to:1)
+            .stroke(.red,style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .bevel, dash: [4,20]) )
+            .frame(width:200, height:50)
+            
     }
 }
 
