b343877ba97ef3f8796fd6ccf93eced76fee04aa
Arunrth <arunrthwm@gmail.com>
2024-10-28 20:28:46 +0530

    RoundedRectangle view

--- IosProject/#1SwiftUIComponents/#6RoundedRectangleView.swift
+++ IosProject/#1SwiftUIComponents/#6RoundedRectangleView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct _6RoundedRectangleView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: 20)
+            .fill(.primary)
+            .frame(width: 200,height: 100)
     }
 }
 
