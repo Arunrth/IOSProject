377299703c8ebfb76677ee24fb2f3a32500d32d3
Arunrth <arunrthwm@gmail.com>
2024-10-28 20:27:22 +0530

    Rectangle view

--- IosProject/#1SwiftUIComponents/#5RectangleViewShape.swift
+++ IosProject/#1SwiftUIComponents/#5RectangleViewShape.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct _5RectangleViewShape: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Rectangle()
+//            .fill(.yellow)
+            //            .foregroundColor(.green)
+//                        .stroke()
+                        .stroke(lineWidth: 2)
+            //            .stroke(.blue)
+            //            .stroke(.red ,style: StrokeStyle(lineWidth: 20, lineCap: .butt, lineJoin: .round, dash: [10,20]))
+            //            .stroke(.blue,style: StrokeStyle(lineWidth:4, lineCap: .round, dash: [3,10]))
+            //            .fill(.yellow)
+            //            .trim(from: 0.2, to: 1)
+            //            .stroke(.green, style: StrokeStyle(lineWidth: 20))
+            .frame(width:200 , height:  100)
     }
 }
 
