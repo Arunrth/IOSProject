65c200f2d44cdd0d5123507508e05f2091073bb5
Arunrth <arunrthwm@gmail.com>
2024-12-19 08:26:14 +0530

    background and overlay

---
+++ IosProject/#1SwiftUIComponents/#12BackgroundAndOverlay.swift
@@ -0,0 +1,18 @@
+//
+//  #12BackgroundAndOverlay.swift
+//  IosProject
+//
+//  Created by Arun Rathore on 03/11/24.
+//
+
+import SwiftUI
+
+struct _12BackgroundAndOverlay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _12BackgroundAndOverlay()
+}
