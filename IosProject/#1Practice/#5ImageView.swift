74d2bd8ed8cf6afa3c2897aedc4b24c213d29a76
Arunrth <arunrthwm@gmail.com>
2025-05-02 10:27:32 +0530

    Image view

---
+++ IosProject/#1Practice/#5ImageView.swift
@@ -0,0 +1,42 @@
+//
+//  #5ImageView.swift
+//  IosProject
+//
+//  Created by Arun Rathore on 02/05/25.
+//
+
+
+import SwiftUI
+
+struct _5ImageView : View {
+    var body: some View {
+        Text("Image View")
+            .font(.largeTitle)
+            .foregroundStyle(.blue)
+        
+        Image("arunPic")
+            .resizable()
+        
+        Image("linkedinIcon")
+        //.renderingMode(.template)
+            .resizable()
+        //.aspectRatio(contentMode: .fit)
+        //.scaledToFit()
+            .scaledToFit()
+            .frame(width: 300, height: 200)
+            .foregroundColor(.green)
+        //.clipped()
+        //.cornerRadius(150)
+        //.clipShape(
+        //Circle()
+        //RoundedRectangle(cornerRadius: 25.0)
+        //Ellipse()
+        //    Circle()
+        //)
+    }
+}
+
+
+#Preview {
+    _5ImageView()
+}
